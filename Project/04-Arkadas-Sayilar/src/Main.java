d7ed2f1665ef176da698033cbdd4e4b25d6be945
Şenol Şen <95684961+senolsn@users.noreply.github.com>
2022-09-21 02:24:48 +0300

    Mini Project 4

---
+++ Project/04-Arkadas-Sayilar/src/Main.java
@@ -0,0 +1,30 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 220-284
+		int number1 = 220;
+		int number2 = 284;
+		int total1 = 0;
+		int total2 = 0;
+
+		for (int i = 1; i < number1; i++) {
+			if (number1 % i == 0) {
+				total1 += i;
+			}
+		}
+
+		for (int j = 1; j < number2; j++) {
+			if (number2 % j == 0) {
+				total2 += j;
+			}
+		}
+		if (number1 == total2 && number2 == total1) {
+			System.out.println("Girilen iki sayı arkadaş sayıdır!");
+		} else {
+			System.out.println("Girilen iki sayı arkadaş sayı DEĞİLDİR!");
+		}
+
+	}
+
+}
